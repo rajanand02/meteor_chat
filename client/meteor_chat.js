943ee641b3f239849858d2a41edd8cbb177b84a3
rajanand02 <raj_anand02@yahoo.in>
2013-11-08 01:13:31 +0530

    rough mockup

---
+++ client/meteor_chat.js
@@ -0,0 +1,18 @@
+Meteor.Router.add({
+  '/': 'home',
+  '/rooms/:id': 'room'
+});
+var rooms = [
+  {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago',
+    messages: [
+      {author: 'Tom', text: 'Hi there Sacha!'},
+      {author: 'Sacha', text: 'Hey Tom, how are you?'},
+      {author: 'Tom', text: 'Good thanks!'},
+    ]},
+  {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
+  {name: 'Meteor Core', members: 0, last_activity: '3 days ago'}
+];
+ 
+Template.room.helpers({
+  room: rooms[0]
+});
