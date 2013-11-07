2ef8a1e3f25ae6a7d244dc93cc4d4218d3f78aa7
rajanand02 <raj_anand02@yahoo.in>
2013-11-08 02:08:22 +0530

    Manual chat inputs

--- client/meteor_chat.js
+++ client/meteor_chat.js
@@ -3,16 +3,12 @@ Meteor.Router.add({
   '/rooms/:id': 'room'
 });
 var rooms = [
-  {name: 'Meteor Talk', members: 3, last_activity: '1 minute ago',
-    messages: [
-      {author: 'Tom', text: 'Hi there Sacha!'},
-      {author: 'Sacha', text: 'Hey Tom, how are you?'},
-      {author: 'Tom', text: 'Good thanks!'},
-    ]},
-  {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
-  {name: 'Meteor Core', members: 0, last_activity: '3 days ago'}
+  {name: 'Rails Development', members: 6, last_activity: '1 minute ago'},
+  {name: 'Meteor Development', members: 2, last_activity: '5 minutes ago'},
+  {name: 'Rails Core', members: 0, last_activity: '3 days ago'},
+  {name: 'Meteor Core', members: 3, last_activity: '6 days ago'}
 ];
- 
-Template.room.helpers({
-  room: rooms[0]
+ 
+Template.rooms.helpers({
+  rooms: rooms
 });
