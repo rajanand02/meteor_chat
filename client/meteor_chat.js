3fa89028ea5d0f708a33d398c327ce85e6961e89
rajanand02 <raj_anand02@yahoo.in>
2013-11-09 07:01:36 +0530

    Add chat without chatroom and configure github

--- client/meteor_chat.js
+++ client/meteor_chat.js
@@ -12,3 +12,29 @@ var rooms = [
 Template.rooms.helpers({
   rooms: rooms
 });
+Template.messages.messages = function(){
+  return Messages.find({}, { sort: { time: -1}});
+}
+
+Template.input.events = {
+  'keydown input#message' : function (event){
+    if (event.which == 13){
+      if (Meteor.user())
+        var name = Meteor.user().profile.name;
+      else
+        var name = "Anonymous";
+      var message = document.getElementById('message');
+
+      if (message.value != ''){
+        Messages.insert({
+          name: name,
+          message: message.value,
+          time: Date.now(),
+        });
+
+        document.getElementById('message').value = '';
+        message.value = '';
+      }
+    }
+  }
+}
